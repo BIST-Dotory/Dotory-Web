b258465035d8c13fcf77fd5e35b230b240876694
dabdev0 <kyoho.song@gmail.com>
2024-12-10 21:57:08 +0900

    update detail

--- src/components/notice/NoticeDetail.vue
+++ src/components/notice/NoticeDetail.vue
@@ -4,34 +4,68 @@
       <h2 class="title">공지사항</h2>
     </div>
     <div class="content">
-      <div class="notice-detail">
+      <div class="detail">
         <div class="detail-header">
           <h3 class="detail-title">{{ notice.title }}</h3>
-          <div class="detail-profile">
-            <div class="detail-profile-image">
+          <div class="profile-container">
+            <div class="profile-image">
               <i class="bi bi-person-circle"></i>
             </div>
-          </div>
-            <span class="detail-profile-name">{{ notice.author }}</span>
-            <div class="detail-info">
-              <span class="detail-info-date">{{ notice.date }}</span>
-              <span class="detail-info-views">조회수 {{ notice.views }}</span>
+            <div class="profile-text">
+              <span class="profile-name">{{ notice.author }}</span>
+              <div class="profile-meta">
+                <span class="profile-date">{{ notice.date }}&nbsp;</span>
+                <span class="profile-views">조회수 {{ notice.views }}</span>
+              </div>
             </div>
-        </div>
-        <div class="detail-content">
-          {{ notice.content }}
-        </div>
-        <div class="button-container">
-          <button class="btn-list" @click="goBack">
-            <i class="bi bi-list"></i>
-            목록
-          </button>
+          </div>
+          <div class="detail-content">
+            {{ notice.content }}
+          </div>
+          <div class="detail-comment">
+            <textarea class="comment-input" placeholder="댓글을 입력하세요"></textarea>
+            <button class="button-comment">등록</button>
+          </div>
+          <div class="comment-item">
+            <div class="comment-avatar">
+              <i class="bi bi-person-circle"></i>
+            </div>
+            <div class="comment-content">
+              <div class="comment-header">
+                <span class="comment-author">{{ comment.author }}</span>
+              </div>
+              <div class="comment-main">
+                <span class="comment-text">{{ comment.text }}</span>
+              </div>
+              <div class="comment-footer">
+                <span class="comment-date">{{ comment.date }}</span>
+              </div>
+            </div>
+          </div>
+          <div class="button-container">
+            <div class="button-container-left">
+              <button class="btn-list" @click="goBack">
+                <i class="bi bi-list"></i>
+                목록
+              </button>
+            </div>
+            <div class="button-container-right">
+              <button class="btn-rewrite" @click="editNotice(notice)">
+                <i class="bi bi-pencil"></i>
+                수정
+              </button>
+              &nbsp;&nbsp;
+              <button class="btn-delete" @click="deleteNotice(notice.id)">
+                <i class="bi bi-trash"></i>
+                삭제
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -40,7 +74,7 @@ export default {
     id: {
       type: String,
       required: true
-    }
+    },
   },
   data() {
     return {
@@ -51,9 +85,16 @@ export default {
         date: '2024.11.19',
         views: 0,
         content: '공지사항 내용입니다.'
-      }
+      },
+      comment: {
+        required: true,
+        author: '사용자',
+        text: '댓글 내용입니다.',
+        date: '2024.11.19 20:00', 
+      },
     }
   },
+
   methods: {
     goBack() {
       this.$router.push({ name: 'Notice' })
@@ -75,28 +116,114 @@ export default {
   margin-bottom: 1rem;
 }
 
-.detail-profile {
-  color: #9A8A80;
+.profile-container {
   display: flex;
+  align-items: center;
+  gap: 1rem;
 }
 
-.detail-profile-name {
-  font-size: 18px;
+.profile-text {
+  margin-top: -10px;
+}
+
+/* 이름 (상단) */
+.profile-name {
+  font-weight: bold;
   color: #4F3322;
-  font-family: 'NanumGothicBold';
 }
 
-.detail-info {
+/* 작성일, 조회수 (하단) */
+.profile-meta {
+  color: #9A8A80;
+}
 
+.detail-content {
+  margin-top: 1rem;
+  margin-bottom: 5rem;
+  white-space: pre-line;
+}
+
+.detail-comment {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #9A8A80;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: #fff;
+  max-width: 100%;
+  margin: 1rem auto;
+}
+
+.comment-input {
+  width: 100%;
+  border: none;
+  outline: none;
+  color: #9B4B1C;
+  padding: 5px;
+  height: 5rem;
+  resize: none;
+}
+
+.button-comment {
+  align-self: flex-end;
+  padding: 5px 15px;
+  background-color: #BA7851;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.button-comment:hover {
+  background-color: #9B4B1C;
+}
+
+.comment-input::placeholder {
+  color: #9A8A80;
+}
+
+.comment-item {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 2rem;
+  border-bottom: 1.5px solid #9A8A80;
+}
+
+.comment-content {
+  display: flex;
+  flex-direction: column;
+  color: #4F3322;
   margin-left: 1rem;
+}
+
+.comment-header {
+  font-weight: bold;
+  color: #4F3322;
+  margin: 5px 0 5px 0;
+}
+
+.comment-footer {
+  color: #9A8A80;
+  font-size: 14px;
+  margin-bottom: 0.5rem;
 }
 
 .button-container {
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
 }
 
-.btn-list {
+.button-container-left {
+  display: flex;
+  justify-content: left;
+}
+
+.button-container-right {
+  display: flex;
+  justify-content: right;
+}
+
+.btn-list, .btn-rewrite, .btn-delete {
   display: flex;
   align-items: center;
   gap: 5px;
@@ -104,21 +231,22 @@ export default {
   color: white;
   border: none;
   border-radius: 5px;
-  padding: 8px 20px;
-  font-family: 'NanumGothic';
+  padding: 5px 15px;
   cursor: pointer;
   transition: background-color 0.3s;
 }
 
-.btn-list:hover {
+.btn-list:hover, .btn-rewrite:hover, .btn-delete:hover {
   background-color: #9B4B1C;
 }
 
-.bi-list {
+.bi-list, .bi-pencil, .bi-trash {
   font-size: 20px;
+  margin-top: 5px;
 }
 
 .bi-person-circle {
   font-size: 50px;
+  color: #9A8A80;
 }
 </style>
